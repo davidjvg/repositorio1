66d7ca4df8a00d799ba92fd921cf235afd71c3e4
davidjvg <36414398+davidjvg@users.noreply.github.com>
2019-10-29 11:21:08 +0100

    new file:   ejemplo.py

---
+++ ejemplo.py
@@ -0,0 +1,7 @@
+edad= input("¿Cuál es tu edad? > ")
+morir= 70
+import datetime()
+date=datetime.datetime.now()
+print('Tu año de nacimiento es ' + str(2019-(int(edad))))
+print('la palmarás en' '\t'*1 + str(int(morir)-(int(edad))))
+print ('y ahora mismo es ' (date))
